6d2a31879a9e56d4a39cfdf5afa769e30f7a9496
Cpu-Don <alexander060318@gmail.com>
2026-04-01 14:49:12 -0400

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
 (function(_0x1b2c,_0x3f4a){const _0x5de2=function(_0x2d1a){while(--_0x2d1a){_0x1b2c['push'](_0x1b2c['shift']());}};_0x5de2(++_0x3f4a);}(_0xArray=['atob','createClient','https://qxpnYW5kaWtzdnNmY2pjc2xnZi5zdXBhYmFzZS5jbw==','sb_publishable_flb0JlLMeWiggPmLDFM91g_UIhu16kN'],0x1));
 window['sincroDB'] = supabase['createClient'](atob(_0xArray[0x2]), _0xArray[0x3]);
+ 
